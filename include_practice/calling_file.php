4599cd0681e1de7a73320aad808712c17ae7f6db
mariamnozadze <mariamnozadze97@yahoo.com>
2022-07-01 21:57:31 +0400

    - include, substr practice tests

---
+++ include_practice/calling_file.php
@@ -0,0 +1,9 @@
+<html>
+
+<body>
+    <h1>Welcome to Jia's cars!</h1>
+    <?php include 'variables.php';
+    echo "i have a $color $car"; ?>
+</body>
+
+</html>
---
+++ include_practice/variables.php
@@ -0,0 +1,4 @@
+<?php
+
+$color = 'black';
+$car = 'Mustang';
